6281779ab0fcdc344ed37bdaa82cacba303664f2
sholq <semyon.silkin@gmail.com>
2022-08-07 16:19:42 +0300

    feat: add basic promo markup

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,7 +2,9 @@
 
 function Promo() {
   return (
-    <div />
+    <div className="promo">
+      <h1 className="promo__header">Учебный проект студента факультета Веб-разработки</h1>
+    </div>
   );
 }
 
