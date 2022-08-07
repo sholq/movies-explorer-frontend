2a3d6060273e566c3c367eb14d15bbbdae9f5761
sholq <semyon.silkin@gmail.com>
2022-08-07 15:20:07 +0300

    feat: add basic header markup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+// import './Header.css';
+
+function Header() {
+  return (
+    <div />
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+// import './Main.css';
+
+import Promo from "../Promo/Promo";
+import Header from "../Header/Header";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <Promo />
+    </>
+  );
+}
+
+export default Main;
