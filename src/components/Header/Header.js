a23f229ceb21cb61835977b69edd2829e3f6337a
sholq <semyon.silkin@gmail.com>
2022-08-07 15:25:50 +0300

    fix: fix basic header markup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
 // import './Header.css';
 
+import logoPath from "../../images/logo.svg";
+import {Link} from "react-router-dom";
+
 function Header() {
   return (
-    <div />
+    <header className="header">
+      <img className="header__logo" src={logoPath} alt="Логотип" />
+      <div>
+        <Link className="header__link header__link_sign_in" to="/sign-in">Войти</Link>
+        <Link className="header__link header__link_sign_up" to="/sign-up">Регистрация</Link>
+      </div>
+    </header>
   );
 }
 
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,16 +1,8 @@
 // import './Promo.css';
 
-import {Link} from "react-router-dom";
-
 function Promo() {
   return (
-    <header className="header">
-      <img className="header__logo" src={logoPath} alt="Логотип Место" />
-      <div>
-        <Link className="header__link header__link_sign_in" to="/sign-in">Войти</Link>
-        <Link className="header__link header__link_sign_up" to="/sign-up">Регистрация</Link>
-      </div>
-    </header>
+    <div />
   );
 }
 
