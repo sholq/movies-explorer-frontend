c4e1a2126c8eb7b0f540b45025830f097c28c2b2
sholq <semyon.silkin@gmail.com>
2022-08-14 18:17:31 +0300

    feat: add SearchMovies markup and styles

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,32 @@
+import {useState} from "react";
+
 function SearchForm() {
+  const [isOff, setIsOff] = useState(false);
+
+  const handleSwitchClick = () => {
+    setIsOff(() => !isOff);
+    console.log("Click");
+  }
+
   return (
     <section className="search">
       <div className="section__container section__container_type_search">
-        <div className="search__bar">
-          <label className="search__wrap">
+        <div className="search__wrap">
+          <form className="search__form">
             <div className="search__icon" />
-            <input className="search__input"/>
-          </label>
-          <button className="search__enter"/>
-          <button className="search__filter"/>
+            <label className="search__field">
+              <input className="search__input" placeholder="Фильм"/>
+            </label>
+            <button className="search__enter" type="submit"/>
+          </form>
+          <div className="search__filter">
+            <div className={"search__hub" + (isOff ? " search__hub_off" : "")} onClick={handleSwitchClick}>
+              <button className={"search__switch" + (isOff ? " search__switch_off" : "")}/>
+            </div>
+            <p className="search__caption">Короткометражки</p>
+          </div>
         </div>
+
       </div>
     </section>
   );
